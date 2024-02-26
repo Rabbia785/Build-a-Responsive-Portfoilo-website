5d67dd7cbcae371946b70be00da4447f6b1beb71
Rabbia <rabbiarabbia645@gmail.com>
2024-02-26 23:38:51 +0500

    pizza website
    pizza website build

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className='footer'>
+        <div className='logo1'>
+        <img src="/img/logo.png" alt='logo'/>
+        </div>
+        <div className='footer1'>
+        <ul>
+            <li>
+                <a href=''>Email: rabbiarabbia645@gmail.com</a>
+            </li>
+            <li>
+                <a href=''>Phone no: +923136990785</a>
+            </li>
+        </ul>
+           
+        </div>
+            
+        </div>
+    );
+}
+
+export default Footer;
